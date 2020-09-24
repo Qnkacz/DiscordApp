5b0f4b2aa2a767a853d66b6b66eaa8e7b057813b
Qnkacz <wasik.bartosz@outlook.com>
2020-09-24 20:10:17 +0200

    tutaj masz commita dlaczego nie działa castowanie na dnd powodzenia

--- DiscordApp/Program.cs
+++ DiscordApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiscordApp.RPGSystems.DnD;
+using System;
+using System.IO;
 
 namespace DiscordApp
 {
@@ -7,7 +9,15 @@ namespace DiscordApp
         static void Main(string[] args)
         {
             var bot = new Bot();
-            bot.RunAsync().GetAwaiter().GetResult();
+           // bot.RunAsync().GetAwaiter().GetResult();
+            string JsonFromFile = string.Empty;
+            using (var reader = new StreamReader("C:\\Users\\ufus0\\source\\repos\\DiscordApp\\DiscordApp\\bin\\Debug\\netcoreapp3.1\\358599651710074880\\DnD\\nigga.json"))
+            {
+
+                JsonFromFile = reader.ReadToEnd();
+            }
+            Console.WriteLine(JsonFromFile);
+            DnD character = Newtonsoft.Json.JsonConvert.DeserializeObject<DnD>(JsonFromFile);
         }
     }
 }
